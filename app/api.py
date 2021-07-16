576f5ce2ff07da90af0bc9920a380312cc064276
snehilk31 <85320012+snehilk31@users.noreply.github.com>
2021-07-16 03:58:58 -0400

    o

--- app/api.py
+++ app/api.py
@@ -1,6 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from . import crud, models
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.get("/")
-def welcome():
-  x={"message":"HELLO WORLD !!! Welcome to Okteto !!"}
-  return x
+def welcome(db: Session=Depends(get_db)):
+    x = crud.get_salary(db)
+    return x
