53be14fc549cebefb4a5969a1338af0ca6a6b813
snehilk31 <85320012+snehilk31@users.noreply.github.com>
2021-07-29 20:24:18 -0400

    aa

--- app/crud.py
+++ app/crud.py
@@ -3,4 +3,4 @@ from sqlalchemy.orm import Session
 from . import models
 
 def get_salary(db: Session):
-    return db.query(models.salary.player, models.salary.position, models.salary.team).all()
+    return db.query(models.salary.player, models.salary.position, models.salary.team, models.salary.salary).all()
