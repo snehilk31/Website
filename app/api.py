eb6853e5eb92f8047025c1b278ee1af3e1880114
snehilk31 <85320012+snehilk31@users.noreply.github.com>
2021-08-19 16:59:06 -0400

    o

--- app/api.py
+++ app/api.py
@@ -58,4 +58,38 @@ async def welcome(request: Request, db: Session=Depends(get_db)):
         margin=dict(l=0, r=0, t=0, b=0))
     teamsalary = figteam.to_html(full_html=False, include_plotlyjs='cdn')
 
-    return templates.TemplateResponse("chart.html", {"request": request, "top10": top10, "team10": team10})
+    pos10 = dfteam.groupby('Position')['Salary'].mean().sort_values(ascending=False).head(10)
+    pos10 = pos10.reset_index()
+    figpos = px.box(dfteam.loc[dfteam['Position'].isin(pos10.Position)], x='Position', y='Salary')
+    figpos.update_layout(yaxis = dict(tickfont = dict(size=5)),
+        xaxis = dict(tickfont = dict(size=5)),
+        font=dict(size=5),
+        margin=dict(l=0, r=0, t=0, b=0))
+    possalary = figpos.to_html(full_html=False, include_plotlyjs='cdn')
+
+    bottom10 = df.groupby('Team')['Salary'].sum()
+    bottom10 = bottom10.reset_index()
+    bottom10 = dfteam.sort_values('Salary', ascending=False).tail(10)
+    dfteam = df.loc[df['Team'].isin(bottom10.Team)]
+
+    pos10 = dfteam.groupby('Position')['Salary'].mean().sort_values(ascending=False).head(10)
+    pos10 = pos10.reset_index()
+    figpos2 = px.box(dfteam.loc[dfteam['Position'].isin(pos10.Position)], x='Position', y='Salary', color_discrete_sequence=['red'])
+    figpos2.update_layout(yaxis = dict(tickfont = dict(size=5)),
+        xaxis = dict(tickfont = dict(size=5)),
+        font=dict(size=5),
+        margin=dict(l=0, r=0, t=0, b=0))
+    possalary2 = figpos2.to_html(full_html=False, include_plotlyjs='cdn')
+
+    dfteam = df.groupby('Position')['Salary'].mean()
+    dfteam = dfteam.reset_index()
+    dfteam = dfteam.sort_values('Salary', ascending=False)
+
+    figpie = px.pie(dfteam, values='Salary', names='Position')
+    figpie.update_layout(yaxis = dict(tickfont = dict(size=5)),
+        xaxis = dict(tickfont = dict(size=5)),
+        font=dict(size=5),
+        margin=dict(l=0, r=0, t=0, b=0))
+    pospie = figpie.to_html(full_html=False, include_plotlyjs='cdn')
+
+    return templates.TemplateResponse("chart.html", {"request": request, "top10": top10, "team10": team10, "teamsalary":teamsalary, "possalary":possalary, "possalary2":possalary2, "pospie":pospie})
